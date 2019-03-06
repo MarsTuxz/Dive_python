1cb8ebe38e5b2c3fc23a4c358e7c6adac511cdff
mars <18270880901@163.com>
2019-03-06 10:10:41 +0800

    fill the day03

--- chapter01/day03.py
+++ chapter01/day03.py
@@ -10,7 +10,11 @@
 # import pandas as pd
 import re
 
-if __name__ == '__main__':
+
+'''
+正则表达式的基本使用
+'''
+def re_use():
     s = '100 road oroad'
     # $ 表示以只在末尾替换
     new_s = re.sub('road$', 'rd.', s)
@@ -26,7 +30,7 @@ if __name__ == '__main__':
     print(re.search(pattern, 'mmm'))
     print(re.search(pattern, 'mmmm'))
     # {}表示匹配的次数的区间
-    pattern1= '^m{0,4}$'
+    pattern1 = '^m{0,4}$'
     print(re.search(pattern1, 'mmmm'))
     print(re.search(pattern1, 'mmm'))
     #
@@ -35,3 +39,23 @@ if __name__ == '__main__':
     print(re.search(pattern2, 'mmf'))
     print(re.search(pattern2, 'mmn'))
     re.findall()
+
+
+def foo(args):
+    x = 1
+    print(locals())
+    # locals 命名空间不会改变局部的参数的值， 他只是对参数的一个拷贝
+    locals()['x'] = 2
+    print(locals())
+
+if __name__ == '__main__':
+    foo(7)
+    foo('bar')
+    print(globals())
+    z = 10
+    #
+    # globals 命名空间会改变全局的变量的值，并且还可以新增加新的变量。
+    globals()['y'] = 100
+    globals()['z'] = 1000
+    print(globals())
+
